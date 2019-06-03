ab91cc5ee3618e3786b7083719d2908db9a88a03
May <may@Gato-Pro.local>
2019-06-03 18:23:33 +0700

    form todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  ListGroup,
+  ListGroupItem,
+  Button,
+  Form,
+  Input,
+  InputGroup
+} from "reactstrap";
+
+const initialTodoList = [
+  "Teach Intro to React Hooks",
+  "Get coffee",
+  "Fix all the bugs"
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <section>
+      <h1>TODO</h1>
+      <Form>
+        <InputGroup>
+          <Input />
+          <Button>Add</Button>
+        </InputGroup>
+      </Form>
+      <ListGroup>
+        {initialTodoList.map((item, i) => {
+          return <ListGroupItem key={i}>{item}</ListGroupItem>;
+        })}
+      </ListGroup>
+    </section>
   );
 }
 
